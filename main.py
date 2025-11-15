7c6d78c8ba7bb6d88728d92bd2b0bac6b2e6036c
nikhilydv45 <nikhil971791@gmail.com>
2025-11-15 13:31:03 +0530

    main.py

--- main.py
+++ main.py
@@ -1,67 +1,501 @@
-import tkinter as tk
-from tkinter import messagebox
+# print("Hello, World!") 
+# x = 0
+# y = "Hello, World!"
+# print(x)
+# print(bool(x))
+# print(y)
+# print(type(y))
 
-#Function
-def add_task():
-    task = entry.get()
-    if task != "":
-        listbox.insert(tk.END,task)
-        entry.delete(0,tk.END)
-    else:
+# my_var=43
+# print(my_var)
 
-        messagebox.showwarning("Warning","Task cannot be empty!")
-
-def remove_task():
-    try:
-        selected = listbox.curselection()[0]
-        listbox.delete(selected)
-    except IndexError:
-        messagebox.showwarning("Warning","Please select a task to remove!")
-
-def update_task():
-    try:
-        selected = listbox.curselection()[0]
-        new_task = entry.get()
-        if new_task!= "":
-            listbox.delete(selected)
-            listbox.insert(selected,new_task)
-            entry.delete(0,tk.END)
-        else:
-            messagebox.showwarning("Warning","Updated task cannot be empty!")
-    except ImportError:
-        messagebox.showwarning("Warning","Please select a task to be update!")
-
-def exit_app():
-    root.destroy()
+# x,y,z="1","2","3"
+# x = "345","67","678"
+# print(x,y,z)
+# print(type(x))
 
 
-#GUI Setup
-
-root=tk.Tk()
-root.title("To-Do List App")
-root.geometry("400x400")
-root.config(bg="#f5f5f5")
-
-
-#Input field
-
-entry = tk.Entry(root,width=30,font=("Arial",14))
-entry.pack(pady=10)
+# x = "Python"
+# y = "67"
+# z = "3.14"
+# print(x)
+# print(y)
+# print(z)
+# print(type(x))
+# print(type(y))
+# print(type(z))
 
 
-#Buttons
-btn_frame = tk.Frame(root,bg="#f5f5f5")
-btn_frame.pack(pady=5)
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# print(type(fruits))
+
+# #LIST 
+# x  = [1,2,3,4,5]
+# print(x)
+# print(type(x))
+
+# #TUPLE
+
+# x = (1,2,3,4,5)
+# print(x)
+# print(type(x))
+
+# #SET
+# x = {1,2,3,4,5}
+# print(x)
+# print(type(x))
+
+# #DICTIONARY
+# x = {"name":"John", "age":30, "city":"New York"}
+# print(x)
+# print(type(x))
+
+# #BOOLEAN
+# x = True
+# print(x)
+# print(type(x))
+
+# #FLOAT
+# x = 3.14
+# print(x)
+# print(type(x))
+# #INTEGER
+# x = 42
+# print(x)
+# print(type(x))
+# #STRING
+# x = "Hello, World!"
+# print(x)
+# print(type(x))
+
+# #short
+# list=[1,2,3,4,5]
+# list.sort()
+# print(list)
+
+# list.sort(reverse=False)
+# print(list)
+
+# list.sort(reverse=True)
+# print(list)
+
+# #extend
+# list1=[1,2,3,4,5]
+# list2=[6,7,8,9,0]
+# list1.extend(list2)
+# print(list1)
+
+# #insert
+# list3=[1,2,3,4,5]
+# list3.insert(2,10)
+# print(list3)
+
+# #remove
+# list4=[1,2,3,4,5]
+# list4.remove(3)
+# print(list4)
+
+# #pop
+# list5=[1,2,3,4,5]
+# list5.pop()
+# print(list5)
+# list5.pop(1)
+# print(list5)
+
+# #append
+# list6=[1,2,3,4,5]
+# list6.append(6)
+# print(list6)
+
+# #clear
+# list7=[1,2,3,4,5]
+# list7.clear()
+# print(list7)
+
+# text="hello"
+# print(text.upper())
+# print(text.lower())
+# print(text.strip())
+# print(text.replace("hello","world"))
+# print(text.split())
+
+# x = "bob"
+# y = 20 
+# print(f"my name is {x} and I am {y} years old")
+
+# # Arithmetic operators
+# x= 10
+# x += 5
+# print(x)
+# x -= 3
+# print(x)
+# x *= 2
+# print(x)
+# x /= 4
+# print(x)
+# x %= 3
+# print(x)
+# x = 10
+# x //= 3
+# print(x)
+# # Comparison operators
+# x = 2
+# y = 3
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+# # Logical operators
+# print(x < 5 and y > 2)
+# print(x < 5 or y > 5)
+# print(not(x < 5 and y > 2))
+
+# # Identity operators
+# a = ["apple", "banana"]
+# b = a
+# print(a is b
+# )
+# print(a is not b)
+# c = ["apple", "banana"]
+# print(a is c)
+# print(a is not c)
+
+# # Membership operators
+# fruits = ["apple", "banana", "cherry"]
+# print("banana" in fruits)
+# print("grape" not in fruits)
+# print("banana" not in fruits)
+# print("grape" in fruits)
+
+# colors = ["red", "green", "blue"]
+# for color in colors:
+#     print(color)
+# for i in range(5):
+#     print(i)
 
 
-tk.Button(btn_frame,text="Add Task", width=10,command=add_task).grid(row=0,column=0,padx=5)
+# #if else condition
+# age = 20 
+# if age <= 18:
+#     print("You are eligible to vote")
 
-tk.Button(btn_frame,text="Remove Task", width= 10, command=remove_task).grid(row=0,column=2,padx=5)
-tk.Button(root,text="Exit",width=10,command=exit_app).pack(pady=5)
+# else:
+#     print("You are not eligible to vote")
 
-#task list
+# marks = 85
+# if marks >= 90:
+#         print("A grade")
+# elif marks >= 80:
+#         print("B grade")
+# elif marks >= 70:
+#         print("C grade")
+# else:
+#         print("Fail")
 
-listbox=tk.Listbox(root,width=40,height=10,font=("arial",12))
-listbox.pack(pady=10)
+# marks = int(input("Enter your marks: "))
+# print(f"Your marks are {marks}")
+# if marks >= 90:
+#         print("A grade")
+# elif marks >= 80:
+#         print("B grade")
+# elif marks >= 70:
+#         print("C grade")
+# else:
+#         print("Fail")
 
-root.mainloop()
+# x = 10
+
+# if x>10:
+#    print("x is greater than 15")
+
+#    if x>20:
+#       print("x is greater than 20")
+#    else:
+#         print("x is not greater then 20")
+
+# x = int(input("Enter a x: "))
+
+# if x%2==0:
+#     print("x is even")
+# else:
+#     print("x is odd")  
+
+# y = int(input("Enter a x: "))
+# if x>0:
+#     print("x is positive")
+# elif x<0:
+#     print("x is negative")
+
+# else:
+#     print("x is zero")
+
+# z = int(input("Enter a x: "))
+# if x>=18:
+#     print("Your are eeligillble to drive a car")
+# else:
+#     print("Your are not eligible to drive a car")
+
+
+# #loops
+
+# word = "Python"
+
+# for latter in word:
+#     print(latter)
+
+
+#     for i in range(5):
+#         print(i)
+
+# for i in range(2,10,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(1,4):
+#   for j in range(1,3):
+     
+#      print(f"i={i},j={j}")
+
+# break in for loop 
+
+# for i in range(1,6):
+
+#   if i==4:
+
+#     break
+  
+#   print(i)
+
+
+# continue in for loop 
+
+# for i in range(1,6):
+  
+#   if i==5:
+
+#     continue
+  
+#   print(i)
+
+
+# QUESTIONS 
+
+# for i in range (1,20):
+#  print(i)
+
+# for i in range (2,30,2):
+#  print(i)
+
+# colors = ("RED","BLUE","YELLOW")
+
+# for color in colors:
+#     print(color)
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i*j, end="\t")
+#     print()
+
+# def greet():
+
+#     print("hello, python")
+
+# greet()
+
+# def greet(name):
+#     print(f"HELLO, {name}!")  
+
+# greet("Alice")
+# greet("Bob")
+
+# def greet(name="Student"):
+    
+#     print(f"Hello,{name}!")
+
+# greet()
+
+# return 
+
+# def add(a,b):
+    
+#     return a+b
+
+# result= add (5,3)
+# print(result)
+
+
+
+# def multiply(a,b):
+    
+#     return a*b
+
+# result= multiply(5,3)
+# print(result)
+
+
+# def divide(a,b):
+    
+#     return a/b
+
+# result= divide (5,3)
+# print(result)
+
+
+# def substract(a,b):
+    
+#     return a-b
+
+# result= substract (5,3)
+# print(result)
+
+
+
+# # scope of variable
+# x = 10  # global variable
+# def my_function():
+#     y = 5  # local variable
+#     print("Inside",x,y)
+
+
+# my_function()
+# print("Outside",x)
+# print(x)
+# #print(y) # error
+
+# #questions
+# #1
+# def greet(name = "Student"):
+#     print(f"Hello, {name}!")
+
+# greet()
+
+# #2
+# def subtract(a, b):
+#     return a - b
+
+# result = subtract(10, 5)
+# print(result)
+# #7
+# x = 10 
+# def my_function():
+#     y = 5  
+#     print("Inside",x,y)
+
+
+# my_function()
+# print("Outside",x)
+# print(x)
+
+
+# oop's in python
+# class Car:
+#     def __init__(self, brand,color):
+#         self.brand = brand
+#         self.color = color
+
+#     def drive(self):
+#         print(f"{self.color}{self.brand} is driving 🛻")
+
+# #object
+# car1=Car("Toyota","Red")
+# car2=Car("Honda","Blue")
+# car1.drive()
+# car2.drive()
+
+
+# array
+
+# ARRAY MODULE
+# import array
+
+# # create an array of integers 
+# numbers = array.array('i',[1,2,3,4,5])
+# print(numbers)
+
+# # access elements
+# import numbers
+# numbers = [1, 2, 3, 4, 5]
+
+# print(numbers[0])  # first element
+# print(numbers[2])  # third element
+# print(numbers[-1]) # last element
+
+# #random number generaye in numpy 
+
+# from numpy import random 
+
+# x=random.randint(100)
+# print(x)
+# print(type(x))
+
+# x=1+random.rand()
+# print(x)
+# print(type(x))
+
+
+# from numpy import random
+
+
+# x =random.randint(100,size=(5))
+# print(x)
+
+# x=random.randint(100,size=(3,5))
+# print(x)
+# x=random.randint(100,size=(2,3,4))
+# print(x)
+# x=random.randint(100,size=(2,2,3,5))
+# print(x)
+
+# PANDAS
+# import pandas as ld
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+# df = ld.DataFrame(data)
+# print(df)
+
+# import pandas as pd
+
+# data=[10,20,30,40,50]
+# s=pd.Series(data)
+
+# print(s)
+
+# import pandas as pd
+
+
+# data={
+#     "Name":["Alice","Bob","Charlie","David"],
+#     "Age":[24,27,22,32],
+#     "City":["Delhi","Mumbai","Chennia","Kolkata"]
+# }
+
+# df=pd.DataFrame(data)
+
+
+# print(df)
+
+# import pandas as pd
+
+
+# arr=np.array([1,2,3,4,5])
+# s=pd.Series(arr)
+# print(s)
+
+import pandas as pd
+
+data={"Math":90,"Science":85,"Engish":78}
+
+s=pd.Series(data)
+
+print(s)
